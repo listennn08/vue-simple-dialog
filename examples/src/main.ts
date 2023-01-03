5fbd12ed7fb3ed5ad84a5b360e431552adf5dc3b
Matt Tai <listennn08@outlook.com>
2023-01-03 14:29:45 +0800

    chore: rename

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueSimpleDialog from 'vue-simple-dialog'
-import 'vue-simple-dialog/dist/index.css'
+import VueSimpleConfirm from 'vue-simple-confirm'
+import 'vue-simple-confirm/dist/index.css'
 
 const app = createApp(App)
 
-app.use(VueSimpleDialog).mount('#app')
+app.use(VueSimpleConfirm).mount('#app')
